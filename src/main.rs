78fc0378506215b936df0230fbcb14dab2ff7536
datzero9 <nguyenmanhdat2903@gmail.com>
2026-05-06 19:58:15 +0700

    GET and SET command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use std::sync::Arc;
+
+use bytes::Bytes;
 use codecrafters_redis::resp::RespParser;
 use codecrafters_redis::types::RedisValueRef;
+use dashmap::DashMap;
 use futures::{SinkExt, StreamExt};
 use tokio::net::TcpListener;
 use tokio_util::codec::Framed;
@@ -7,14 +11,15 @@ use tokio_util::codec::Framed;
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-
+    let storage = Arc::new(DashMap::<Bytes, Bytes>::new());
     loop {
         match listener.accept().await {
             Ok((stream, _addr)) => {
+                let storage = Arc::clone(&storage);
                 tokio::spawn(async move {
                     let mut framed = Framed::new(stream, RespParser::default());
                     while let Some(Ok(value)) = framed.next().await {
-                        let response = handle_command(value);
+                        let response = handle_command(value, &storage);
                         if framed.send(response).await.is_err() {
                             break;
                         }
@@ -26,7 +31,7 @@ async fn main() {
     }
 }
 
-fn handle_command(value: RedisValueRef) -> RedisValueRef {
+fn handle_command(value: RedisValueRef, storage: &Arc<DashMap<Bytes, Bytes>>) -> RedisValueRef {
     let RedisValueRef::Array(parts) = value else {
         return RedisValueRef::ErrorMsg(b"ERR expected array command".to_vec());
     };
@@ -36,11 +41,51 @@ fn handle_command(value: RedisValueRef) -> RedisValueRef {
     };
 
     match cmd.to_ascii_uppercase().as_slice() {
-        b"PING" => RedisValueRef::SimpleString(bytes::Bytes::from_static(b"PONG")),
+        b"PING" => RedisValueRef::SimpleString(Bytes::from_static(b"PONG")),
         b"ECHO" => match parts.get(1) {
             Some(RedisValueRef::BulkString(msg)) => RedisValueRef::BulkString(msg.clone()),
-            _ => RedisValueRef::ErrorMsg(b"ERR wrong number of arguments for 'echo' command".to_vec()),
+            _ => RedisValueRef::ErrorMsg(
+                b"ERR wrong number of arguments for 'echo' command".to_vec(),
+            ),
         },
+        b"SET" => {
+            if parts.len() != 3 {
+                return RedisValueRef::ErrorMsg(
+                    b"ERR wrong number of arguments for 'set' command".to_vec(),
+                );
+            }
+
+            let key = match parts.get(1) {
+                Some(RedisValueRef::BulkString(msg)) => msg.clone(),
+                _ => return RedisValueRef::ErrorMsg(b"ERR invalid key type".to_vec()),
+            };
+
+            let value = match parts.get(2) {
+                Some(RedisValueRef::BulkString(msg)) => msg.clone(),
+                _ => return RedisValueRef::ErrorMsg(b"ERR invalid value type".to_vec()),
+            };
+
+            storage.insert(key, value);
+
+            RedisValueRef::SimpleString(Bytes::from_static(b"OK"))
+        }
+        b"GET" => {
+            if parts.len() != 2 {
+                return RedisValueRef::ErrorMsg(
+                    b"ERR wrong number of arguments for 'get' command".to_vec(),
+                );
+            }
+
+            let key = match parts.get(1) {
+                Some(RedisValueRef::BulkString(key)) => key,
+                _ => return RedisValueRef::ErrorMsg(b"ERR invalid key type".to_vec()),
+            };
+
+            match storage.get(key) {
+                Some(entry) => RedisValueRef::BulkString(entry.value().clone()),
+                None => RedisValueRef::NullBulkString,
+            }
+        }
         _ => RedisValueRef::ErrorMsg(b"ERR unknown command".to_vec()),
     }
 }
